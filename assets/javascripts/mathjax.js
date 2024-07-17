0c87a9a94460ac936c230a1b2c501f632292f501
mkdocs-publisher <9740637+mkusz@users.noreply.github.com>
2024-07-17 22:41:32 +0000

    Update from https://github.com/mkdocs-publisher/mkdocs-publisher.github.io/commit/c93e18ee7a73b9464a0ed887adefe99c89af8958

---
+++ assets/javascripts/mathjax.js
@@ -0,0 +1,1 @@
+window.MathJax={tex:{inlineMath:[["\\(","\\)"]],displayMath:[["\\[","\\]"]],processEscapes:!0,processEnvironments:!0},options:{ignoreHtmlClass:".*|",processHtmlClass:"arithmatex"}},document$.subscribe(()=>{MathJax.startup.output.clearCache(),MathJax.typesetClear(),MathJax.texReset(),MathJax.typesetPromise()});
